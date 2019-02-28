9147086b8d6087f79dce88f33bfa570d32431f54
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-02-28 14:25:41 +0530

    Modified Fact to use Theory with InlineData for parameterized unit testing.

--- MathLibTestsXUnit/CalculatorTests.cs
+++ MathLibTestsXUnit/CalculatorTests.cs
@@ -5,12 +5,16 @@ namespace MathLibTestsXUnit
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void AddShouldReturnSomeOfValues()
+        [Theory]
+        [InlineData(5, 2, 3)]
+        [InlineData(2, 5, -3)]
+        [InlineData(10, 4, 6)]
+        [InlineData(0, 5, -5)]
+        public void AddShouldReturnSomeOfValues(int expected, int val1, int val2)
         {
             var calculator = new Calculator();
-            var result = calculator.Add(2, 3);
-            Assert.Equal(5, result);
+            var result = calculator.Add(val1, val2);
+            Assert.Equal(expected, result);
         }
     }
 }
