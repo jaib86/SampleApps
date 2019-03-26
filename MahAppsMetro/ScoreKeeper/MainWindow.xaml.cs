3be7776aac16507dc27cbed0c74c754cdbd55542
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-03-26 16:04:12 +0530

    Added project ScoreKeeper.

---
+++ MahAppsMetro/ScoreKeeper/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MahApps.Metro.Controls;
+using ScoreKeeper.ViewModels;
+
+namespace ScoreKeeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.DataContext = new MainWindowViewModel(NavigateToView);
+        }
+
+        private void NavigateToView(UserControl view)
+        {
+            MainArea.Content = view;
+        }
+    }
+}
