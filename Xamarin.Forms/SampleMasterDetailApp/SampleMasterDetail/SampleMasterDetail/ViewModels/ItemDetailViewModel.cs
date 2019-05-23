e0237b6cfcee79dc1a74a649fd53d0a992705609
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-05-23 18:46:39 +0530

    SampleMasterDetailApp

---
+++ Xamarin.Forms/SampleMasterDetailApp/SampleMasterDetail/SampleMasterDetail/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SampleMasterDetail.Models;
+
+namespace SampleMasterDetail.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
