2d1cc259875e434f1619d2936d98b05875d31f3c
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-06-05 10:14:08 +0530

    Added ITestOutputHelper

--- AspNetCoreWeb/WebAppsIntegrationTests/BasicTests.cs
+++ AspNetCoreWeb/WebAppsIntegrationTests/BasicTests.cs
@@ -2,16 +2,19 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace WebAppsIntegrationTests
 {
     public class BasicTests : IClassFixture<WebApplicationFactory<RazorPagesProject.Startup>>
     {
         private readonly HttpClient client;
+        private readonly ITestOutputHelper output;
 
-        public BasicTests(WebApplicationFactory<RazorPagesProject.Startup> factory)
+        public BasicTests(WebApplicationFactory<RazorPagesProject.Startup> factory, ITestOutputHelper output)
         {
             this.client = factory.CreateClient();
+            this.output = output;
         }
 
         [Fact]
@@ -22,6 +25,9 @@ namespace WebAppsIntegrationTests
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            this.output.WriteLine($"{nameof(HttpResponseMessage.StatusCode)}: {response.StatusCode}");
+
             Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
         }
     }
