b1ae505f09958baec601d993dda4c0f4d41508d2
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-07-02 09:44:58 +0530

    Added SingletonClass and its test class in Design Patterns project.

---
+++ DesignPatterns/DesignPatterns.Tests/SingletonTests.cs
@@ -0,0 +1,34 @@
+﻿using Factory;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DesignPatterns.Tests
+{
+    public class SingletonTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public SingletonTests(ITestOutputHelper outputHelper)
+        {
+            this.output = outputHelper;
+        }
+
+        [Fact]
+        public void ShouldCreateJustOneInstance()
+        {
+            SingletonClass first = SingletonClass.Instance;
+            SingletonClass second = SingletonClass.Instance;
+            this.output.WriteLine($"First instance hashcode: {first.GetHashCode()}");
+            this.output.WriteLine($"Second instance hashcode: {second.GetHashCode()}");
+            Assert.Same(first, second);
+            first.SomeValue++;
+            this.output.WriteLine($"First instance {nameof(SingletonClass.SomeValue)}: {first.SomeValue}");
+            this.output.WriteLine($"Second instance {nameof(SingletonClass.SomeValue)}: {second.SomeValue}");
+            Assert.Equal(first.SomeValue, second.SomeValue);
+            second.SomeValue++;
+            this.output.WriteLine($"First instance {nameof(SingletonClass.SomeValue)}: {first.SomeValue}");
+            this.output.WriteLine($"Second instance {nameof(SingletonClass.SomeValue)}: {second.SomeValue}");
+            Assert.Equal(first.SomeValue, second.SomeValue);
+        }
+    }
+}
---
+++ DesignPatterns/Factory/SingletonClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Factory
+{
+    public sealed class SingletonClass : IDisposable
+    {
+        private readonly bool disposed;
+
+        // The volatile keyword ensure tat the instantiation is complete
+        // before it can be accessed further helping with thread safety.
+        private static volatile SingletonClass instance;
+
+        private static readonly object syncLock = new object();
+
+        private SingletonClass()
+        {
+        }
+
+        // Double check locking pattern
+        public static SingletonClass Instance
+        {
+            get
+            {
+                if (instance != null) return instance;
+
+                lock (syncLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new SingletonClass();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+
+            // This object will be cleaned up by the Dispose method.
+            // Therefore, you should call GC.SuppressFinalize to
+            // take this object off the finalization queue
+            // and prevent finalization code for this object
+            // from executing a second time.
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            // Check to see if Dispose has already been called.
+            if (this.disposed) return;
+            // If disposing equals true, dispose all managed
+            // and unmanaged resources
+            if (disposing)
+            {
+                instance = null;
+                // Dispose managed resources
+            }
+
+            // Call appropriate methods to clean up
+            // unmanaged resources here.
+        }
+
+        public int SomeValue { get; set; }
+    }
+}
