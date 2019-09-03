3b5033a9cbe8927a512bda7ea8d6f7321f0b5616
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2019-09-03 11:23:32 +0530

    Added project "Library.API"

---
+++ AspNetCoreWeb/API/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = this.libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
---
+++ AspNetCoreWeb/API/Library.API/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Library.API.Entities;
+using Library.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Library.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var connectionString = this.Configuration.GetConnectionString("libraryDBConnectionString");
+            services.AddDbContextPool<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            // register the repository
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, LibraryContext libraryContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+                app.UseHsts();
+            }
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
