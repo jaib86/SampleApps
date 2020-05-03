a4adc0febc42d3104e8374f50ad98a011c4eaa9e
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2020-05-03 18:16:58 +0530

    Part 24: Blazor Event Handling -- Added @onmousemove and @onclick event handlers -- Dynamically adding css class to component

--- KudVenkat/Blazor/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ KudVenkat/Blazor/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -2,6 +2,7 @@
 using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace EmployeeManagement.Web.Pages
 {
@@ -15,9 +16,32 @@ namespace EmployeeManagement.Web.Pages
 
         public Employee Employee { get; set; }
 
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        protected void OnMouseMove(MouseEventArgs e)
+        {
+            Coordinates = $"X = {e.ClientX}, Y = {e.ClientY}";
+        }
+
+        protected void OnButtonClick()
+        {
+            if(ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                ButtonText = "Hide Footer";
+                CssClass = null;
+            }
         }
     }
 }
