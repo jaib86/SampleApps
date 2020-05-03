e1fd7d077ae3feb8fb182bdcf186005b56946107
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2020-05-03 09:48:42 +0530

    Part 22: Blazor Route Parameters -- Added EmployeeDetails.razor and EmployeeDetailsBase.cs -- Added [Parameter] decorator to get the router parameter from component to component base class -- Added GetEmployee() method in EmployeeService.

---
+++ KudVenkat/Blazor/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        [Parameter]
+        public string Id { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
