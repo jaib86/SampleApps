3f6c1236b45db8628ee625a268a592cdcccd2578
jaiprakashbarnwal <jaiprakash.barnwal@nagarro.com>
2020-05-03 19:03:15 +0530

    Part 26: A simple Blazor two way data binding example

--- KudVenkat/Blazor/EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
+++ KudVenkat/Blazor/EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
@@ -6,6 +6,9 @@ namespace EmployeeManagement.Web.Pages
     {
         protected string Name { get; set; } = "Tom";
         protected string Gender { get; set; } = "Male";
+
         protected string Colour { get; set; } = "background-color:white";
+
+        protected string Description { get; set; } = string.Empty;
     }
 }
